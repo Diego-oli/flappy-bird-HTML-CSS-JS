528e87810b5d88187f8938ca30614181a3d16ea2
Alamito <alamirbobroski12@gmail.com>
2022-10-18 16:51:48 -0300

    feat - add jump or fall of the bird

--- script/script.js
+++ script/script.js
@@ -1,27 +1,39 @@
 const bird = document.querySelector('.bird');
 const varCSS = document.documentElement;    // acessa campo de var do style.css
+let flagClick = true;
 
 let valueBottom = getComputedStyle(bird).getPropertyValue('bottom');    //pega valor atual de bottom do bird
 let valuePositionMoment = getComputedStyle(varCSS).getPropertyValue('--positionMoment');
 let valuePositionJumpFinal = getComputedStyle(varCSS).getPropertyValue('--positionJumpFinal');
 
 const jump = () => {
-    varCSS.style.setProperty('--positionMoment', parseInt(valueBottom) + 'px');
-    varCSS.style.setProperty('--positionJumpFinal', parseInt(valueBottom) + 100 + 'px');
-    updateValue();
 
-    bird.classList.add('jumpBird');
+    if (flagClick === true) {
+        updateValue();
+        varCSS.style.setProperty('--positionMoment', parseInt(valueBottom) + 'px');
+        varCSS.style.setProperty('--positionJumpFinal', parseInt(valueBottom) + 100 + 'px');
+        bird.classList.add('jumpBird');
+        bird.classList.remove('fallBird');
+        flagClick = false;
+    }
+    
+    setTimeout(() => {
+        updateValue();
+        bird.classList.remove('jumpBird');
+        bird.classList.add('fallBird');
+        flagClick = true;
+    }, 500);
 
-    console.log(valueBottom);
-    console.log(valuePositionMoment);
-    console.log(valuePositionJumpFinal);
+
+    // console.log(valueBottom);
+    // console.log(valuePositionMoment);
+    // console.log(valuePositionJumpFinal);
 }
+
+document.addEventListener('click', jump);
 
 function updateValue() {
     valueBottom = getComputedStyle(bird).getPropertyValue('bottom');    //pega valor atual de bottom do bird
     valuePositionMoment = getComputedStyle(varCSS).getPropertyValue('--positionMoment');
     valuePositionJumpFinal = getComputedStyle(varCSS).getPropertyValue('--positionJumpFinal');
 }
-
-
-document.addEventListener('click', jump);
